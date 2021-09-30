4dd7d95d7da6d59aae19537ea0d8b5431a32b1f9
Paige <72361304+pckellyco@users.noreply.github.com>
2021-09-29 21:18:13 -0600

    Added ideas for components in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,9 @@ import '../stylesheets/NavBar.css'
 export default function NavBar() {
     return (
         <nav>
+            // Before Tech
+            // Current Tech or Tech Now or Tech
+            // Future Tech
             <a>What I know</a>
             <a>What I am learning</a>
             <a>What I have done</a>
