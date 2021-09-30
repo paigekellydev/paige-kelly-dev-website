7909196c69e5ad0b8856c3305e0b9743a71c2ac8
Paige <72361304+pckellyco@users.noreply.github.com>
2021-09-29 20:53:14 -0600

    Created new stylesheet for loading component and transferred styling from App.css

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../stylesheets/Loading.css';
 
 export default function Loading() {
     return (
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <nav>
+            <a>What I know</a>
+            <a>What I am learning</a>
+            <a>What I have done</a>
+            <a>Who I am</a>
+        </nav>
+    )
+}
